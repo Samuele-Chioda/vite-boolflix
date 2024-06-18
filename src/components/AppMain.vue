7fac8bd92c44c2d5704aa64b20b2e9bfab5aa1aa
Samuele-Chioda <samuelechioda@gmail.com>
2024-06-18 19:04:31 +0200

    Modifiche a AppMain.vue

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -31,6 +31,7 @@
 <script>
 import { computed } from 'vue';
 import { state, setItems, setQuery } from '../store/store';
+import axios from 'axios'; 
 
 export default {
     name: 'AppMain',
@@ -49,37 +50,52 @@ export default {
         const searchItems = async () => {
             console.log('Searching for:', query.value);
             if (query.value.trim() !== '') {
-                const movieResponse = await fetch(
-                    `https://api.themoviedb.org/3/search/movie?api_key=${state.apiKey}&query=${query.value}&language=it-IT`
-                );
-                const movieData = await movieResponse.json();
+                try {
+                    const movieResponse = await axios.get(
+                        `https://api.themoviedb.org/3/search/movie`, {
+                            params: {
+                                api_key: state.apiKey,
+                                query: query.value,
+                                language: 'it-IT'
+                            }
+                        }
+                    );
 
-                const tvResponse = await fetch(
-                    `https://api.themoviedb.org/3/search/tv?api_key=${state.apiKey}&query=${query.value}&language=it-IT`
-                );
-                const tvData = await tvResponse.json();
+                    const tvResponse = await axios.get(
+                        `https://api.themoviedb.org/3/search/tv`, {
+                            params: {
+                                api_key: state.apiKey,
+                                query: query.value,
+                                language: 'it-IT'
+                            }
+                        }
+                    );
 
-                const movies = movieData.results.map(movie => ({
-                    id: movie.id,
-                    title: movie.title,
-                    original_title: movie.original_title,
-                    original_language: movie.original_language,
-                    vote_average: movie.vote_average,
-                    type: 'movie',
-                    poster_path: movie.poster_path
-                }));
+                    const movies = movieResponse.data.results.map(movie => ({
+                        id: movie.id,
+                        title: movie.title,
+                        original_title: movie.original_title,
+                        original_language: movie.original_language,
+                        vote_average: movie.vote_average,
+                        type: 'movie',
+                        poster_path: movie.poster_path
+                    }));
 
-                const tvShows = tvData.results.map(tv => ({
-                    id: tv.id,
-                    title: tv.name,
-                    original_title: tv.original_name,
-                    original_language: tv.original_language,
-                    vote_average: tv.vote_average,
-                    type: 'tv',
-                    poster_path: tv.poster_path
-                }));
+                    const tvShows = tvResponse.data.results.map(tv => ({
+                        id: tv.id,
+                        title: tv.name,
+                        original_title: tv.original_name,
+                        original_language: tv.original_language,
+                        vote_average: tv.vote_average,
+                        type: 'tv',
+                        poster_path: tv.poster_path
+                    }));
 
-                setItems([...movies, ...tvShows]);
+                    setItems([...movies, ...tvShows]);
+                } catch (error) {
+                    console.error('Error fetching data:', error);
+                    setItems([]);
+                }
             } else {
                 setItems([]);
             }
@@ -102,7 +118,7 @@ export default {
 
         const getPosterUrl = (posterPath) => {
             const baseUrl = 'https://image.tmdb.org/t/p/';
-            const size = 'w342'; // Puoi cambiare la dimensione se desideri
+            const size = 'w342';  // dimensione
             return `${baseUrl}${size}${posterPath}`;
         };
 
@@ -174,13 +190,12 @@ button:hover {
 }
 
 .flag {
-    width: 10%;
-    height: 12px;
+    width: 5%;
+    height: 15px;
 }
 
 .poster {
     width: 100px;
     height: auto;
 }
-
 </style>
